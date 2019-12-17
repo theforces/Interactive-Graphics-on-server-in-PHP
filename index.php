93289adcec7785bb5624922d85ccdc686fb255fa
theforces <32979080+theforces@users.noreply.github.com>
2019-12-17 18:24:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    echo '<html>';
+    echo '<head>';
+    echo '<title>image filling</title>';
+    echo '<style> body { background-image: url("/images/coffee1.png"); background-position: 50% 50%; background-repeat: repeat; } </style>';
+    echo '</head>';
+    echo '<body>';
+    echo '<h2></h2>';
+    echo '</body>';
+    echo '</html>';
+?>
